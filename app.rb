5957b9d01b3ee6f5650ac484b98155163a6edae2
rumikoacopa <rumikorios@gmail.com>
2020-08-22 21:31:14 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,48 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
 
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number ** 2}"
+  end
+
+  get '/say/:number/:phrase' do
+    @say_what = []
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    @number.times{@say_what << @phrase}
+    "#{@say_what.join()}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word_1 = params[:word1]
+    @word_2 = params[:word2]
+    @word_3 = params[:word3]
+    @word_4 = params[:word4]
+    @word_5 = params[:word5]
+    "#{@word_1} #{@word_2} #{@word_3} #{@word_4} #{@word_5}" + "."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    case @operation
+    when "add"
+      "#{@number1 + @number2}"
+    when "subtract"
+      "#{@number2 - @number1}"
+    when "divide"
+      "#{@number1 / @number2}"
+    when "multiply"
+      "#{@number1 * @number2}"
+    end
+  end
 end
+
+
